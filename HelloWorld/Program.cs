c7010115ee67b553559444ff38e3f1546e0509a9
Artur Mochnia <amochnia@gmail.com>
2023-12-06 17:22:23 +0000

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Main(args, Console.WriteLine("Nazywam się Artur Mochnia"));
+    }
+
+    public static void Main(string[] args, void v)
+    {
+        string firstAndLastName = "Artur";
+        Console.WriteLine("Hello, World!");
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
+public interface IProgram
+{
+
+}
